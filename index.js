295925d84f0a344a52289c1d470801d57e13e904
Anastassia Troitskaia <atroitskaia@gmail.com>
2017-11-22 19:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ function theBeatlesPlay(musicians, instruments) {
   let instrument =[];
 for (musician = 0; musician<musicians.length; musician++) {
   return musicians[musician];
+for (instrument = 0; instrument<instruments.length; instrument++) {
+  return instruments[instrument];
+}
 }
 
 
